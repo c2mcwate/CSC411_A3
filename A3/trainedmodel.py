2052862c36ed21f6e9b42418b168048b20852f4b
Matt <matt.mcwaters@mail.utoronto.ca>
2015-12-10 16:23:02 -0500

    Finished all models

--- A3/trainedmodel.py
+++ A3/trainedmodel.py
@@ -29,7 +29,10 @@ from skimage.morphology import disk
 from skimage.exposure import *
 from skimage import filters
 from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
-
+import cv2
+from skimage.color import rgb2gray
+from sklearn import mixture
+from sklearn import linear_model
 
 def save_object(obj, filename):
 
@@ -42,6 +45,8 @@ def SVM(submit):
     labeled_images_data = spio.loadmat("labeled_images.mat")
     unlabeled_images_data = spio.loadmat("unlabeled_images.mat")
     public_test_data = spio.loadmat("public_test_images.mat")
+    hidden_test_data = spio.loadmat("hidden_test_images.mat")
+    hidden_faces = hidden_test_data.get("hidden_test_images")
     faces_test = public_test_data.get("public_test_images")
     unlabeled_faces = unlabeled_images_data.get("unlabeled_images")
     labels = labeled_images_data.get("tr_labels")
@@ -49,6 +54,8 @@ def SVM(submit):
     faces = labeled_images_data.get("tr_images")
     faces = faces.transpose(2, 0, 1)
     faces = faces.reshape((faces.shape[0], -1))
+    hidden_faces = hidden_faces.transpose(2, 0, 1)
+    hidden_faces = hidden_faces.reshape((hidden_faces.shape[0], -1))
 
     unlabeled_faces = unlabeled_faces.transpose(2, 0, 1)
     unlabeled_faces = unlabeled_faces.reshape((unlabeled_faces.shape[0], -1))
@@ -60,45 +67,45 @@ def SVM(submit):
     #train_data, test_data, train_targets, test_targets, train_ident, test_ident = train_test_split(faces, labels_s, identities, train_size=0.9)
     #test = np.intersect1d(train_ident, test_ident)
 
-    small_faces = faces
-    small_identities = identities
-    small_labels = labels_s
-    aug = np.column_stack((small_identities, small_labels,small_faces))
+    # small_faces = faces
+    # small_identities = identities
+    # small_labels = labels_s
+    # aug = np.column_stack((small_identities, small_labels,small_faces))
+    #
+    # one_array = np.array(filter(lambda row: row[1]==1, aug))
+    # two_array = np.array(filter(lambda row: row[1]==2, aug))
+    # three_array = np.array(filter(lambda row: row[1]==3, aug))
+    # four_array = np.array(filter(lambda row: row[1]==4, aug))
+    # five_array = np.array(filter(lambda row: row[1]==5, aug))
+    # six_array = np.array(filter(lambda row: row[1]==6, aug))
+    # seven_array = np.array(filter(lambda row: row[1]==7, aug))
+    #
+    # label_arrays = [one_array, two_array, three_array, four_array, five_array, six_array, seven_array]
+    #
+    # for j in range(len(label_arrays)):
+    #     label_arrays[j] = label_arrays[j][label_arrays[j][:,0].argsort()[::-1]]
+    #
+    #
+    # master_array = aug.copy()
+    #
+    # #save_object(label_arrays, "label_arrays")
+    # # label_arrays = load_object("label_arrays")
+    #
+    # i = 0
+    # while i < len(faces):
+    #     for j in range(len(label_arrays)):
+    #         if i < len(faces) and len(label_arrays[j]>0):
+    #             if(j==6):
+    #                  master_array[i] = label_arrays[j][0]
+    #                  label_arrays[j] = np.delete(label_arrays[j] , 0, axis=0)
+    #                  i = i+1
+    #             master_array[i] = label_arrays[j][0]
+    #             label_arrays[j] = np.delete(label_arrays[j] , 0, axis=0)
+    #             #label_arrays[j] = np.zeros(3)
+    #             i = i+1
+    # #save_object(master_array, "master_canny_100-201")
 
-    one_array = np.array(filter(lambda row: row[1]==1, aug))
-    two_array = np.array(filter(lambda row: row[1]==2, aug))
-    three_array = np.array(filter(lambda row: row[1]==3, aug))
-    four_array = np.array(filter(lambda row: row[1]==4, aug))
-    five_array = np.array(filter(lambda row: row[1]==5, aug))
-    six_array = np.array(filter(lambda row: row[1]==6, aug))
-    seven_array = np.array(filter(lambda row: row[1]==7, aug))
-
-    label_arrays = [one_array, two_array, three_array, four_array, five_array, six_array, seven_array]
-
-    for j in range(len(label_arrays)):
-        label_arrays[j] = label_arrays[j][label_arrays[j][:,0].argsort()[::-1]]
-
-
-    master_array = aug.copy()
-
-    #save_object(label_arrays, "label_arrays")
-    # label_arrays = load_object("label_arrays")
-
-    i = 0
-    while i < len(faces):
-        for j in range(len(label_arrays)):
-            if i < len(faces) and len(label_arrays[j]>0):
-                if(j==6):
-                     master_array[i] = label_arrays[j][0]
-                     label_arrays[j] = np.delete(label_arrays[j] , 0, axis=0)
-                     i = i+1
-                master_array[i] = label_arrays[j][0]
-                label_arrays[j] = np.delete(label_arrays[j] , 0, axis=0)
-                #label_arrays[j] = np.zeros(3)
-                i = i+1
-    #save_object(master_array, "master_canny_100-201")
-
-    # master_array = load_object("master")
+    master_array = load_object("master")
 
     master_ident = master_array[:,0]
     master_array = np.delete(master_array,0,1)
@@ -126,45 +133,69 @@ def SVM(submit):
 
     # PUT YOUR PROCESSING HERE
     #Reshape
+    hidden_faces= preprocessing.normalize(hidden_faces, norm='l2')
     master_faces = preprocessing.normalize(master_faces, norm='l2')
+    faces_test = preprocessing.normalize(faces_test, norm='l2')
+    hidden_faces = hidden_faces.reshape(len(hidden_faces), 32, 32)
 
     master_faces = master_faces.reshape(len(master_faces), 32, 32)
-    plt.subplot(122),plt.imshow(master_faces[0], cmap='gray')
+    faces_test = faces_test.reshape(len(faces_test), 32, 32)
+    plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
     plt.title('Normal'), plt.xticks([]), plt.yticks([])
-    plt.show()
+    # plt.show()
+
+
 
     #Gamma correction
-    master_faces = all_gamma(master_faces)
-    plt.subplot(122),plt.imshow(master_faces[0], cmap='gray')
-    plt.title('Gamma correction'), plt.xticks([]), plt.yticks([])
-    plt.show()
+    hidden_faces = all_gamma(hidden_faces)
 
-    #Dog filter
-    master_faces -= scipy.ndimage.filters.gaussian_filter(master_faces, 0.2)
-    plt.subplot(122),plt.imshow(master_faces[0], cmap='gray')
-    plt.title('Dog Filter'), plt.xticks([]), plt.yticks([])
-    plt.show()
+    master_faces = all_gamma(master_faces)
+    faces_test = all_gamma(faces_test)
+    plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
+    plt.title('Gamma correction'), plt.xticks([]), plt.yticks([])
+    # plt.show()
+
+    # #Dog filter
+    # master_faces -= cv2.GaussianBlur(master_faces, (3, 3),1)
+    # faces_test -= cv2.GaussianBlur(faces_test, (3, 3),1)
+    # plt.subplot(122),plt.imshow(faces_test[1], cmap='gray')
+    # plt.title('Dog Filter'), plt.xticks([]), plt.yticks([])
+    # plt.show()
+
+    # #Rescale intensity
+    # master_faces = testing(master_faces)
+    # faces_test = testing(faces_test)
+    #
+    # plt.subplot(122),plt.imshow(faces_test[15], cmap='gray')
+    # plt.title('Rescale'), plt.xticks([]), plt.yticks([])
+    # plt.show()
 
     #Equalization of variance TODO
+    hidden_faces = EQ(hidden_faces)
     master_faces = EQ(master_faces)
-    plt.subplot(122),plt.imshow(master_faces[0], cmap='gray')
+
+    faces_test = EQ(faces_test)
+    plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
     plt.title('Equalization'), plt.xticks([]), plt.yticks([])
-    plt.show()
+    # plt.show()
 
     #Reshape
     master_faces = master_faces.reshape((master_faces.shape[0], -1))
+    faces_test = faces_test.reshape((faces_test.shape[0], -1))
+    hidden_faces = hidden_faces.reshape((hidden_faces.shape[0], -1))
 
     tuples = kfold(master_faces,master_labels,master_ident, 13)
     success_rates_train = []
     success_rate_valid = []
     if not submit:
         for tuple in tuples:
+
             train_data, test_data, train_targets, test_targets, train_ident, test_ident= tuple
             # train_data = pca.transform(train_data)
             # test_data = pca.transform(test_data)
 
-            model = svm.SVC( gamma=0.5, C=1, kernel='poly')
-            #model = BaggingClassifier(classifier, n_estimators=10, bootstrap=True, verbose=1)
+            classifier = svm.SVC( gamma=0.5, C=1, kernel='poly')
+            model = BaggingClassifier(classifier, n_estimators=10, bootstrap=True, verbose=1)
             model.fit(train_data, train_targets)
 
 
@@ -192,9 +223,10 @@ def SVM(submit):
         print(np.average(success_rate_valid))
     if submit:
         classification = svm.SVC( gamma=0.5, C=1, kernel='poly')
-        model = BaggingClassifier(classification, n_estimators=20, bootstrap=True, verbose=1)
+        model = BaggingClassifier(classification, n_estimators=20, bootstrap_features=True, bootstrap=True, verbose=1)
         model.fit(master_faces, master_labels)
         test_predictions = model.predict(faces_test)
+        hidden_predictions = model.predict(hidden_faces)
 
 
         #Test predictions
@@ -204,34 +236,40 @@ def SVM(submit):
         for i in range(len(ascending)):
              ascending[i]=i+1
         ascending = ascending.astype(int)
-        hidden_guesses = -1*np.ones(1253-len(test_predictions))
+        hidden_guesses = hidden_predictions
         test_predictions = np.concatenate([test_predictions, hidden_guesses])
         test_predictions = test_predictions.astype(int)
         csv = np.column_stack((ascending, test_predictions))
-        np.savetxt("test_faces_normal.csv", csv, delimiter=",")
+        np.savetxt("hidden.csv", csv, delimiter=",")
     return
 
-
+def testing(array):
+    for i in range(len(array)):
+        array[i] = exposure.rescale_intensity(array[i])
+    return array
 
 def all_gamma(array):
     for i in range(len(array)):
-        array[i] = exposure.adjust_gamma(array[i], 1)
+        array[i] = exposure.adjust_gamma(array[i], 0.2)
     return array
 
 def EQ(array):
     for i in range(len(array)):
-        array[i] = exposure.equalize_hist(array[i])
         array[i] = equalize(array[i], disk(20))
     return array
 
+def adapt(array):
+    for i in range(len(array)):
+        p2, p98 = np.percentile(array[i], (2, 98))
+        array[i] = exposure.rescale_intensity(array[i], in_range=(p2, p98))
 
-
+    return array
 
 def KNN(submit):
     labeled_images_data = spio.loadmat("labeled_images.mat")
     unlabeled_images_data = spio.loadmat("unlabeled_images.mat")
     public_test_data = spio.loadmat("public_test_images.mat")
-    faces_test = public_test_data.get("public_test_images")
+    faces_test = public_test_data.get("hidden_images")
     unlabeled_faces = unlabeled_images_data.get("unlabeled_images")
     labels = labeled_images_data.get("tr_labels")
     identities = labeled_images_data.get("tr_identity")
@@ -265,6 +303,33 @@ def KNN(submit):
     # print("-   Finished PCA reduction    -")
     #
     # print('PCA captures {:.2f} percent of the variance in the dataset'.format(pca.explained_variance_ratio_.sum() * 100))
+    #Reshape
+    master_faces = preprocessing.normalize(master_faces, norm='l2')
+    faces_test = preprocessing.normalize(faces_test, norm='l2')
+    master_faces = master_faces.reshape(len(master_faces), 32, 32)
+    faces_test = faces_test.reshape(len(faces_test), 32, 32)
+    plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
+    plt.title('Normal'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    #Gamma correction
+    master_faces = all_gamma(master_faces)
+    faces_test = all_gamma(faces_test)
+    plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
+    plt.title('Gamma correction'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+    #Equalization of variance TODO
+    master_faces = EQ(master_faces)
+    faces_test = EQ(faces_test)
+    plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
+    plt.title('Equalization'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    #Reshape
+    master_faces = master_faces.reshape((master_faces.shape[0], -1))
+    faces_test = faces_test.reshape((faces_test.shape[0], -1))
 
     tuples = kfold(master_faces,master_labels,master_ident, 13)
     success_rates_train = []
@@ -272,10 +337,9 @@ def KNN(submit):
     if not submit:
         for tuple in tuples:
             train_data, test_data, train_targets, test_targets, train_ident, test_ident= tuple
-            train_data = pca.transform(train_data)
-            test_data = pca.transform(test_data)
-
-            model = KNeighborsClassifier(n_neighbors=15, weights='distance', algorithm='auto')
+            #train_data = pca.transform(train_data)
+            #test_data = pca.transform(test_data)
+            model = KNeighborsClassifier(n_neighbors=21, weights='distance', algorithm='auto')
             #model = BaggingClassifier(classifier, n_estimators=10, bootstrap=True, verbose=1)
             model.fit(train_data, train_targets)
 
@@ -317,14 +381,12 @@ def KNN(submit):
         for i in range(len(ascending)):
              ascending[i]=i+1
         ascending = ascending.astype(int)
-        hidden_guesses = -1*np.ones(1253-len(test_predictions))
-        test_predictions = np.concatenate([test_predictions, hidden_guesses])
         test_predictions = test_predictions.astype(int)
         csv = np.column_stack((ascending, test_predictions))
-        np.savetxt("test_faces_est30.csv", csv, delimiter=",")
+        np.savetxt("knn.csv", csv, delimiter=",")
     return
 
-def model3(submit):
+def Logit(submit):
     labeled_images_data = spio.loadmat("labeled_images.mat")
     unlabeled_images_data = spio.loadmat("unlabeled_images.mat")
     public_test_data = spio.loadmat("public_test_images.mat")
@@ -352,16 +414,68 @@ def model3(submit):
 
 
 
-    # n_eigenfaces = 121
-    # print("-   Performing PCA reduction    -")
-    # pca = RandomizedPCA(n_components=n_eigenfaces, whiten=True).fit(unlabeled_faces)
-    # save_object(pca, "pca")
-
-    pca = load_object("pca")
-    # train_data = pca.transform(train_data)
-    # test_data = pca.transform(test_data)
+    # PUT YOUR PROCESSING HERE
+    # #Reshape
+    # master_faces = preprocessing.normalize(master_faces, norm='l2')
+    # faces_test = preprocessing.normalize(faces_test, norm='l2')
+    # unlabeled_faces =preprocessing.normalize(unlabeled_faces, norm='l2')
+    # unlabeled_faces= unlabeled_faces.reshape(len(unlabeled_faces), 32, 32)
+    # master_faces = master_faces.reshape(len(master_faces), 32, 32)
+    # faces_test = faces_test.reshape(len(faces_test), 32, 32)
+    # plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
+    # plt.title('Normal'), plt.xticks([]), plt.yticks([])
+    # #plt.show()
     #
+    #
+    #
+    # #Gamma correction
+    # master_faces = all_gamma(master_faces)
+    # faces_test = all_gamma(faces_test)
+    # unlabeled_faces = all_gamma(unlabeled_faces)
+    #
+    # plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
+    # plt.title('Gamma correction'), plt.xticks([]), plt.yticks([])
+    # #plt.show()
+    #
+    # # #Dog filter
+    # # master_faces -= cv2.GaussianBlur(master_faces, (3, 3),1)
+    # # faces_test -= cv2.GaussianBlur(faces_test, (3, 3),1)
+    # # plt.subplot(122),plt.imshow(faces_test[1], cmap='gray')
+    # # plt.title('Dog Filter'), plt.xticks([]), plt.yticks([])
+    # # plt.show()
+    #
+    # # #Rescale intensity
+    # # master_faces = testing(master_faces)
+    # # faces_test = testing(faces_test)
+    # #
+    # # plt.subplot(122),plt.imshow(faces_test[15], cmap='gray')
+    # # plt.title('Rescale'), plt.xticks([]), plt.yticks([])
+    # # plt.show()
+    #
+    # #Equalization of variance TODO
+    # master_faces = EQ(master_faces)
+    # faces_test = EQ(faces_test)
+    # unlabeled_faces = EQ(unlabeled_faces)
+    #
+    # plt.subplot(122),plt.imshow(faces_test[3], cmap='gray')
+    # plt.title('Equalization'), plt.xticks([]), plt.yticks([])
+    # #plt.show()
+    #
+    # #Reshape
+    # master_faces = master_faces.reshape((master_faces.shape[0], -1))
+    # faces_test = faces_test.reshape((faces_test.shape[0], -1))
+    # unlabeled_faces = unlabeled_faces.reshape((unlabeled_faces.shape[0], -1))
+
+
+    n_eigenfaces = 121
+
+    print("-   Performing PCA reduction    -")
+    pca = RandomizedPCA(n_components=121, whiten=True).fit(unlabeled_faces)
+    #save_object(pca, "pca")
+    #train_data = pca.transform(train_data)
+    #test_data = pca.transform(test_data)
     print("-   Finished PCA reduction    -")
+
     print('PCA captures {:.2f} percent of the variance in the dataset'.format(pca.explained_variance_ratio_.sum() * 100))
 
     tuples = kfold(master_faces,master_labels,master_ident, 13)
@@ -370,11 +484,9 @@ def model3(submit):
     if not submit:
         for tuple in tuples:
             train_data, test_data, train_targets, test_targets, train_ident, test_ident= tuple
-            train_data = pca.transform(train_data)
-            test_data = pca.transform(test_data)
-
-            model = SGDClassifier(loss="log", penalty="l2", n_iter=3000, learning_rate='optimal')
-            #model = BaggingClassifier(classifier, n_estimators=10, bootstrap=True, verbose=1)
+            # train_data = pca.transform(train_data)
+            # test_data = pca.transform(test_data)
+            model = linear_model.LogisticRegression(penalty='l2', C=0.5, max_iter=10000)
             model.fit(train_data, train_targets)
 
 
@@ -401,9 +513,10 @@ def model3(submit):
         print("Validation average :")
         print(np.average(success_rate_valid))
     if submit:
+        classification = linear_model.LogisticRegression(penalty='l2', C=0.5, max_iter=10000)
         master_faces = pca.transform(master_faces)
-        classification=SGDClassifier(loss="log", penalty="l2", n_iter=1000, learning_rate='optimal', verbose=True)
-        model = BaggingClassifier(classification, n_estimators=10, bootstrap=True, verbose=1)
+        faces_test = pca.transform(faces_test)
+        model = BaggingClassifier(classification, n_estimators=20, bootstrap=True, verbose=1)
         model.fit(master_faces, master_labels)
         test_predictions = model.predict(faces_test)
 
@@ -419,8 +532,9 @@ def model3(submit):
         test_predictions = np.concatenate([test_predictions, hidden_guesses])
         test_predictions = test_predictions.astype(int)
         csv = np.column_stack((ascending, test_predictions))
-        np.savetxt("sgdc.csv", csv, delimiter=",")
+        np.savetxt("logit.csv", csv, delimiter=",")
     return
+
 def splitSet(data, targets, identities, validRatio):
     "Takes a set of data and returns a validation set and training set"
     number = np.around(len(data)*validRatio)
@@ -459,7 +573,8 @@ def kfold(data, targets, identities, folds):
     return tuples
 
 if __name__ == "__main__":
-    #knn()
+    #KNN(True)
     #semi_supervised()
-    SVM(False)
-    #model3(False)
+    SVM(True)
+
+    #Logit(True)
